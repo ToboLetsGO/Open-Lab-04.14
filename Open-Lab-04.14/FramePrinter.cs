9772c8ff4621a6199daa9d5d8bda6f4e33182022
Tobias Mikula <toboletsgoit@gmail.com>
2021-11-22 14:02:01 +0100

    hotovo

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -4,9 +4,17 @@ namespace Open_Lab_04._14
 {
     public class FramePrinter
     {
-        public void Print(string[] strings)
-        {
-            throw new NotImplementedException();
+        public void Print(string[] strings){
+             int max_lengt = 0;
+            foreach (var item in strings){if (item.Length > max_lengt){ max_lengt = item.Length; }}
+            for (int i = 0; i < max_lengt+4; i++) { Console.Write("*"); }
+            Console.WriteLine();
+            foreach (var item in strings){
+                Console.Write("* ");
+                Console.Write(item);
+                for (int i = 0; i < (max_lengt - item.Length); i++) { Console.Write(" ");}
+                Console.WriteLine(" *");}
+            for (int i = 0; i < max_lengt + 4; i++) { Console.Write("*"); }
         }
     }
 }
